98951708a01aa5affac08cd8b1843c47a5b95608
Angel <ngeljames2409@gmail.com>
2020-12-12 23:08:56 +0530

    final commit

---
+++ onlineSpaBooking.Test/BookingTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using onlineSpaBooking;
+
+namespace onlineSpaBooking.Test
+{
+
+    [TestFixture]
+    public class BookingTest
+
+
+
+
+    {
+        Booking b1 = new Booking(10, "body massage", "combo2", "chennai", 1000, "ayurvedic");
+        [Test]
+
+       
+            // Booking b1 = new Booking(10, "body massage", "combo2", "chennai", 1000, "ayurvedic");
+
+            public void TestBooking()
+            {
+
+
+                Assert.AreEqual(10, b1.BookingId);
+                Assert.AreEqual("body massage", b1.PackName);
+                Assert.AreEqual("combo2", b1.PackType);
+                Assert.AreEqual("chennai", b1.Address);
+                Assert.AreEqual(1000, b1.PackCost);
+                Assert.AreEqual("ayurvedic", b1.PackDetails);
+
+            }
+        
+
+
+    }
+
+}
+
+     
---
+++ onlineSpaBooking.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("onlineSpaBooking.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("onlineSpaBooking.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f31141e-5d94-483d-a861-e3cd1c93e0ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
